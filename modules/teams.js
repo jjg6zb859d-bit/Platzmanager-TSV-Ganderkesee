90de9cadf3e438ba162190e12a41deaf83daa8fd
jjg6zb859d-bit <jjg6zb859d@privaterelay.appleid.com>
2026-07-02 13:13:24 +0200

    Update teams.js

--- modules/teams.js
+++ modules/teams.js
@@ -1,105 +1,28 @@
 function initTeams(){
 
-    renderTeams();
+const div =
+document.getElementById(
+"teamContent"
+);
 
-}
+let html = "";
 
-function renderTeams(){
+APPDATA.teams.forEach(t=>{
 
-    const div =
-    document.getElementById(
-        "teamContent"
-    );
+html += `
 
-    let html = `
+<div class="card">
 
-    <div class="card">
+<h3>${t.name}</h3>
 
-        <h3>Neues Team</h3>
+Team angelegt
 
-        <input
-            id="teamName"
-            placeholder="Team">
+</div>
 
-        <input
-            id="trainerName"
-            placeholder="Trainer">
+`;
 
-        <button
-            onclick="teamAnlegen()">
+});
 
-            Team speichern
-
-        </button>
-
-    </div>
-
-    `;
-
-    APPDATA.teams.forEach(team=>{
-
-        html += `
-
-        <div
-        class="card">
-
-            <h3>${team.name}</h3>
-
-            Trainer:
-            ${team.trainer || "-"}
-
-        </div>
-
-        `;
-
-    });
-
-    div.innerHTML = html;
-
-}
-
-function teamAnlegen(){
-
-    const teamName =
-    document.getElementById(
-        "teamName"
-    ).value;
-
-    const trainer =
-    document.getElementById(
-        "trainerName"
-    ).value;
-
-    if(!teamName){
-
-        alert(
-            "Teamname fehlt"
-        );
-
-        return;
-    }
-
-    APPDATA.teams.push({
-
-        id:
-        neueID(),
-
-        name:
-        teamName,
-
-        trainer:
-        trainer,
-
-        telefon:"",
-        email:"",
-        farbe:"#0054A6"
-
-    });
-
-    speichern();
-
-    renderTeams();
-
-    initDashboard();
+div.innerHTML = html;
 
 }
