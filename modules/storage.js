c0db7489da790cb54a7cb81b9e66604ed156b467
jjg6zb859d-bit <jjg6zb859d@privaterelay.appleid.com>
2026-07-02 11:44:45 +0200

    Update storage.js

--- modules/storage.js
+++ modules/storage.js
@@ -1,1 +1,38 @@
+const APPDATA = {
 
+plaetze:[
+"Hauptplatz A",
+"Hauptplatz B",
+"Hauptplatz C",
+"Hauptplatz D",
+"Kunstrasen Nord",
+"Kunstrasen Süd"
+],
+
+kabinen:[
+"K1 Links",
+"K1 Rechts",
+"K2 Links",
+"K2 Rechts",
+"K3 Links",
+"K3 Rechts",
+"K4 Links",
+"K4 Rechts"
+],
+
+teams:[
+
+{
+name:"D1",
+trainer:"Niels"
+},
+
+{
+name:"C1",
+trainer:"Trainer"
+}
+
+]
+
+};
+`
