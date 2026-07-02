8c95f8f32afaf11a3987ef84ea52c91f9d743b46
jjg6zb859d-bit <jjg6zb859d@privaterelay.appleid.com>
2026-07-02 12:44:42 +0200

    Update plaetze.js

--- modules/plaetze.js
+++ modules/plaetze.js
@@ -1,42 +1,51 @@
-function initPlaetze(){
-
+function initPlaetze() {
     renderPlaetze();
-
 }
 
-function renderPlaetze(){
+function renderPlaetze() {
 
     const div =
-    document.getElementById(
-        "platzMatrix"
-    );
+        document.getElementById("platzMatrix");
 
     let html =
-    '<div class="grid">';
+        '<div class="grid">';
 
-    APPDATA.plaetze.forEach(platz=>{
+    APPDATA.plaetze.forEach(platz => {
+
+        const belegt =
+            APPDATA.reservierungen.filter(
+                r => r.platz === platz.name
+            );
+
+        let status = "frei";
+        let css = "status-frei";
+
+        if (belegt.length > 0) {
+
+            status =
+                `${belegt.length} Belegung(en)`;
+
+            css =
+                "status-belegt";
+        }
 
         html += `
 
-        <div class="card status-frei">
+        <div class="card ${css}">
 
             <h3>
-
-            ${platz.name}
-
+                ${platz.name}
             </h3>
 
-            ${platz.status}
+            <strong>${status}</strong>
 
         </div>
 
         `;
-
     });
 
     html += "</div>";
 
     div.innerHTML =
-    html;
-
+        html;
 }
