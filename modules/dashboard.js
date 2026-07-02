4c71a87b7d168c417a24756d472575dcef3873d2
jjg6zb859d-bit <jjg6zb859d@privaterelay.appleid.com>
2026-07-02 11:45:08 +0200

    Update dashboard.js

--- modules/dashboard.js
+++ modules/dashboard.js
@@ -1,1 +1,41 @@
+function initDashboard(){
+
+const div =
+document.getElementById(
+"dashboardContent"
+);
+
+div.innerHTML=`
+
+<div class="grid">
+
+<div class="card">
+
+<h3>Plätze</h3>
+
+${APPDATA.plaetze.length}
+
+</div>
+
+<div class="card">
+
+<h3>Kabinen</h3>
+
+${APPDATA.kabinen.length}
+
+</div>
+
+<div class="card">
+
+<h3>Teams</h3>
+
+${APPDATA.teams.length}
+
+</div>
+
+</div>
+
+`;
+
+}
 
