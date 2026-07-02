8b24e8cbff2fd8ad85c2b7db52ba00e4ae07c7a4
jjg6zb859d-bit <jjg6zb859d@privaterelay.appleid.com>
2026-07-02 11:45:52 +0200

    Update kabinen.js

--- modules/kabinen.js
+++ modules/kabinen.js
@@ -1,1 +1,32 @@
+function initKabinen(){
 
+const div =
+document.getElementById(
+"kabinenMatrix"
+);
+
+let html =
+'<div class="grid">';
+
+APPDATA.kabinen.forEach(k=>{
+
+html += `
+
+<div class="card status-frei">
+
+<h3>${k}</h3>
+
+frei
+
+</div>
+
+`;
+
+});
+
+html += '</div>';
+
+div.innerHTML = html;
+
+}
+``
