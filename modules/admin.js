0bbaed52a14de1a66fd5bef8aa93ba576b324f07
jjg6zb859d-bit <jjg6zb859d@privaterelay.appleid.com>
2026-07-02 11:46:27 +0200

    Update admin.js

--- modules/admin.js
+++ modules/admin.js
@@ -1,1 +1,24 @@
+function initAdmin(){
 
+const div =
+document.getElementById(
+"adminContent"
+);
+
+div.innerHTML = `
+
+<div class="card">
+
+<h3>Adminbereich</h3>
+
+<p>
+
+V0.9 Vorbereitung
+
+</p>
+
+</div>
+
+`;
+
+}
